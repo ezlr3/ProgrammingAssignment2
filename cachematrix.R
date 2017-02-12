0d93b04a3120eaad0454e7b4028cc94f8e007172
Eric Zimmerman <Eric Zimmerman>
2017-02-12 10:59:25 -0600

    removed test code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## the inverse of the same matrix multiple times.
 
 ## makeCacheMatrix is a function designed to:
-## set the value of a matrix -- valid and square matrices only.  
+## set the value of a matrix 
 ## get the value of a matrix 
 ## set and cache the value of the inverse of this matrix
 ## get the value of the inverse of this matrix
@@ -12,28 +12,9 @@ makeCacheMatrix <- function(x = matrix()) {
   inverseMatrix <- NULL
   
   set <- function(y) {
-    paramClass <- class(y)
+      x <<- y
+      inverseMatrix <<- NULL
 
-    ## Check if parameter passed to the function is a matrix
-    if (paramClass=="matrix") {
-      paramRows <- nrow(y)
-      paramCols <- ncol(y)
-      
-      ## Check if matrix is a square matrix
-      if (paramRows==paramCols) {
-        x <<- y
-        inverseMatrix <<- NULL
-
-      ## Matrix is not a square matrix - return error message.
-      } else {
-        print("Please pass a valid square matrix to the function.")
-      }
-      
-    ## Parameter is not of class matrix - return error message. 
-    } else {
-      print("Please pass a valid matrix to the function.")  
-    }
-    
   }
   
   get <- function() x
